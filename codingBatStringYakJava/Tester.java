56c23e028da4a15b8abcead916695150be4e8895
mayaborkar <66751353+mayaborkar@users.noreply.github.com>
2022-07-21 16:22:05 -0700

    Add files via upload

---
+++ codingBatStringYakJava/Tester.java
@@ -0,0 +1,25 @@
+
+public class Tester
+{
+    
+    public Tester()
+    {
+        stringYak("yakpak");
+    }
+
+    public String stringYak(String str) {
+      String result = "";
+      
+      for (int i=0; i<str.length(); i++) {
+        // Look for i starting a "yak" -- advance i in that case
+        if (i+2<str.length() && str.charAt(i)=='y' && str.charAt(i+2)=='k') {
+          i =  i + 2;
+        } else { // Otherwise do the normal append
+          result = result + str.charAt(i);
+        }
+      }
+      
+      return result;
+    }
+
+}
