0cf2d0a3b0398a2f0f4d775a2a29015e045d1af1
mayaborkar <66751353+mayaborkar@users.noreply.github.com>
2022-10-11 15:02:39 -0700

    New Project

---
+++ GWC Lesson - Running Man.py
@@ -0,0 +1,16 @@
+import pygame
+
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+RED = (255, 0, 0)
+GREEN = (0, 255, 0)
+BLUE = (0, 0, 255)
+
+pygame.init()
+
+pygame.display.set_caption("Maya's Running Man")
+
+width = 970
+height = 520
+
+
---
+++ blueMoon.py
@@ -0,0 +1,3 @@
+'''Problem: find when the next blue moon is with the given information of: when
+the last blue moon was, 27.3 days for a lunar cycle to complete (take into account
+the different amount of days per month'''
