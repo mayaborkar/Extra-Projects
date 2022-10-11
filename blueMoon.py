811a1140f302337d96d6df04e7cd996e1472ae97
mayaborkar <66751353+mayaborkar@users.noreply.github.com>
2022-10-11 15:33:13 -0700

    First steps

--- blueMoon.py
+++ blueMoon.py
@@ -1,3 +1,9 @@
 '''Problem: find when the next blue moon is with the given information of: when
 the last blue moon was, 27.3 days for a lunar cycle to complete (take into account
 the different amount of days per month'''
+
+firstMoonMonth = input("What was the month of the first blue moon? ")
+firstMoonDay = input("What was the day of the first blue moon? ")
+firstMoonYear = input("What was the year of the first blue moon? ")
+
+# add 27.3 continuously until it is in the same month
