9529c69b34bece7fa3317ef4a44a1e33537b1988
mayaborkar <maya.borkar@gmail.com>
2024-08-10 15:27:34 -0700

    sign lang

--- singLanguageDetection.py
+++ singLanguageDetection.py
@@ -3,3 +3,10 @@ import time
 import uuid
 import cv2
 
+IMAGES_PATH = 'Tensorflow/workspace/images/collectedImages'
+
+labels = ['hello', 'thanks', 'yes', 'no', 'iloveyou']
+number_imgs = 15
+
+for label in labels:
+    
