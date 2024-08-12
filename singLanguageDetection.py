c9a0e9194ec8498a483a86baebb4cb76446e9694
mayaborkar <maya.borkar@gmail.com>
2024-08-12 15:21:22 -0700

    capture images

--- singLanguageDetection.py
+++ singLanguageDetection.py
@@ -3,23 +3,23 @@ import time
 import uuid
 import cv2
 
-IMAGES_PATH = 'Tensorflow/workspace/images/collectedImages'
+IMAGES_PATH = '/Users/mayaborkar/Desktop/CollectedImages/'
 
 labels = ['hello', 'thanks', 'yes', 'no', 'iloveyou']
 number_imgs = 15
 
 for label in labels:
-    os.mkdir({'Tensorflow\workspace\images\collectedimages\\' + label}) 
+    os.mkdir('/Users/mayaborkar/Desktop/CollectedImages/' + label) 
     cap = cv2.VideoCapture(0)
     print('Collecting images for {}'.format(label))
     time.sleep(5)
-    for imgnum in range(number_imgs):
+    for imgnum in range(number_imgs): 
         ret, frame = cap.read()
-        imagename = os.path.join(IMAGES_PATH, label, label + '.' + '{}.jpg'.format(str(uuid.uuid1())))
+        imgname = os.path.join(IMAGES_PATH, label, label + '.' + '{}.jpg'.format(str(uuid.uuid1())))
         cv2.imwrite(imgname, frame)
         cv2.imshow('frame', frame)
         time.sleep(2)
 
-        if cv2.waitKey(1) and 0xFF == ord('q'):
+        if cv2.waitKey(1) & 0xFF == ord('q'):
             break
     cap.release()
