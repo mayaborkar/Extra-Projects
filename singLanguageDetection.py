c9834bf6bc96dd82c586e1e0518bc4bed92a3141
mayaborkar <maya.borkar@gmail.com>
2024-08-12 13:04:18 -0700

    sing lang

--- singLanguageDetection.py
+++ singLanguageDetection.py
@@ -9,3 +9,17 @@ labels = ['hello', 'thanks', 'yes', 'no', 'iloveyou']
 number_imgs = 15
 
 for label in labels:
+    os.mkdir({'Tensorflow\workspace\images\collectedimages\\' + label}) 
+    cap = cv2.VideoCapture(0)
+    print('Collecting images for {}'.format(label))
+    time.sleep(5)
+    for imgnum in range(number_imgs):
+        ret, frame = cap.read()
+        imagename = os.path.join(IMAGES_PATH, label, label + '.' + '{}.jpg'.format(str(uuid.uuid1())))
+        cv2.imwrite(imgname, frame)
+        cv2.imshow('frame', frame)
+        time.sleep(2)
+
+        if cv2.waitKey(1) and 0xFF == ord('q'):
+            break
+    cap.release()
