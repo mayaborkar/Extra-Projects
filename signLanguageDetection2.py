0be6b9bdf651bef29cc135b8a02e13ee56649dca
mayaborkar <maya.borkar@gmail.com>
2024-08-21 08:30:34 -0700

    sign lang

--- signLanguageDetection2.py
+++ signLanguageDetection2.py
@@ -1,4 +1,6 @@
 import os
+import shutil
+
 
 WORKSPACE_PATH = '/Users/mayaborkar/Desktop/Tensorflow/workspace'
 SCRIPTS_PATH = WORKSPACE_PATH + '/scripts'
@@ -25,3 +27,44 @@ with open((ANNOTATION_PATH + '\label_map.pbtxt'), 'a') as f:
         f.write("\tid:{}\n".format(label['id']))
         f.write('}\n')
 
+open((ANNOTATION_PATH + '/train.record'), 'a')
+open((ANNOTATION_PATH + '/test.record'), 'a')
+
+
+CUSTOM_MODEL_NAME = 'my_ssd_mobnet' 
+
+os.mkdir(MODEL_PATH + '/' + CUSTOM_MODEL_NAME)
+shutil.copy(os.path.join(PRETRAINED_MODEL_PATH, 'ssd_mobilenet_v2_fpnlite_320x320_coco17_tpu-8', 'pipeline.config'), os.path.join(MODEL_PATH, CUSTOM_MODEL_NAME))
+
+
+print("python {}/research/object_detection/model_main_tf2.py --model_dir={}/{} --pipeline_config_path={}/{}/pipeline.config --num_train_steps=10000""".format(APIMODEL_PATH, MODEL_PATH,CUSTOM_MODEL_NAME,MODEL_PATH,CUSTOM_MODEL_NAME)) 
+
+
+
+# ----- 5. UPDATE CONFIG FOR TRANSFER LEARNING
+import tensorflow as tf
+from object_detection.utils import config_util
+from object_detection.protos import pipeline_pb2
+from google.protobuf import text_format
+
+CONFIG_PATH = MODEL_PATH+'/'+CUSTOM_MODEL_NAME+'/pipeline.config'
+config = config_util.get_configs_from_pipeline_file(CONFIG_PATH)
+config
+
+pipeline_config = pipeline_pb2.TrainEvalPipelineConfig()
+with tf.io.gfile.GFile(CONFIG_PATH, "r") as f:                                                                                                                                                                                                                     
+    proto_str = f.read()                                                                                                                                                                                                                                          
+    text_format.Merge(proto_str, pipeline_config)  
+
+pipeline_config.model.ssd.num_classes = 5
+pipeline_config.train_config.batch_size = 4
+pipeline_config.train_config.fine_tune_checkpoint = PRETRAINED_MODEL_PATH+'/ssd_mobilenet_v2_fpnlite_320x320_coco17_tpu-8/checkpoint/ckpt-0'
+pipeline_config.train_config.fine_tune_checkpoint_type = "detection"
+pipeline_config.train_input_reader.label_map_path= ANNOTATION_PATH + '/label_map.pbtxt'
+pipeline_config.train_input_reader.tf_record_input_reader.input_path[:] = [ANNOTATION_PATH + '/train.record']
+pipeline_config.eval_input_reader[0].label_map_path = ANNOTATION_PATH + '/label_map.pbtxt'
+pipeline_config.eval_input_reader[0].tf_record_input_reader.input_path[:] = [ANNOTATION_PATH + '/test.record']
+
+config_text = text_format.MessageToString(pipeline_config)                                                                                                                                                                                                        
+with tf.io.gfile.GFile(CONFIG_PATH, "wb") as f:                                                                                                                                                                                                                     
+    f.write(config_text)
